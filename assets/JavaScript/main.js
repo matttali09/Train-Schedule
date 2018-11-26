2291dcbfbf9cf1a89dcb0b5a7e44055608485c83
matttali09 <matttaliancich09@gmail.com>
2018-11-26 09:19:55 -0500

    added more styling to the main page

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -17,9 +17,9 @@ $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
     // Grab user input and format the train start in moment js proper format (returned as string)
-    var trainName = $("#employee-name-input").val().trim();
+    var trainName = $("#train-name-input").val().trim();
     var dest = $("#destination-input").val().trim();
-    var trainStart = moment($("#start-input").val().trim(), "HH:mm").format("X");
+    var trainStart = moment($("#start-input").val().trim(), "HH:mm");
     var trainRate = $("#rate-input").val().trim();
 
     // Create local "temporary" object for holding employee data
@@ -64,24 +64,33 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log(trainStart);
     console.log(trainRate);
 
+    // make sure time of user input comes before current time for calculations by subtracting a year
+    var firstTimeConverted = moment(trainStart, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
+    // take the difference in time between the current time and the converted time
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // Calculate the months worked using hardcore math
-    // To calculate the months worked
-    // var empMonths = moment().diff(moment(empStart, "X"), "months");
-    // console.log(empMonths);
+    // take the modulus for next train calculations
+    var tRemainder = diffTime % trainRate;
+    console.log(tRemainder);
 
-    // Calculate the total billed rate
-    // var empBilled = empMonths * empRate;
-    // console.log(empBilled);
+    // find the minutes until next train by subtracting remainder
+    var tMinutesTillTrain = trainRate - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // find the time of the next train by adding the minutes until next to the current time.
+    var trainArival = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
+    console.log("ARRIVAL TIME: " + moment(trainArival).format("hh:mm"));
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(dest),
         $("<td>").text(trainRate),
-        $("<td>").text(nextArrival),
-        $("<td>").text(minutesAway)
+        $("<td>").text(trainArival),
+        $("<td>").text(tMinutesTillTrain)
     );
 
     // Append the new row to the table
